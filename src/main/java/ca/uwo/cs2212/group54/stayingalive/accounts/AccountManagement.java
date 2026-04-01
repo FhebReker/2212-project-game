3a59fd320c0cee3bc65d830b348b59d404a11913
Osman Mohammed Idris <oidris@uwo.ca>
2026-04-01 14:36:35 -0400

    fixed checkuserlogin method

---
+++ src/main/java/ca/uwo/cs2212/group54/stayingalive/accounts/AccountManagement.java
@@ -0,0 +1,21 @@
+package ca.uwo.cs2212.group54.stayingalive.accounts;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class AccountManagement {
+    private char[] masterPass;
+
+    public AccountManagement() {}
+
+    public boolean checkUserLogin(String username, String password) {
+        char[] charPass = password.toCharArray();
+
+        // NOTE: DO NOT TURN getAccount() TO STATIC
+        // MAKE A PARENTAL OBJECT IN THE MAIN CLASS OF THE FILE.
+        Account acc = Parental.getAccount(username);
+        return acc.getPassword().equals(password);
+    }
+
+    
+}
