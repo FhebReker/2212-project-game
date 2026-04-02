67ba002173a3dbae866f5630410a3a5ba27d3c52
Osman Mohammed Idris <oidris@uwo.ca>
2026-04-02 15:31:03 -0400

    added json saving ability to account

--- src/main/java/ca/uwo/cs2212/group54/stayingalive/accounts/Account.java
+++ src/main/java/ca/uwo/cs2212/group54/stayingalive/accounts/Account.java
@@ -1,37 +1,90 @@
 package ca.uwo.cs2212.group54.stayingalive.accounts;
 
-public class Account {
-    private String username;
-    private char[] pass;
-    private LevelStatistic levelStats;
-    public PlayerProgress playerProgress;
-    public int coins;
-    public Sprite playerSprite;
-    public Powerup[] powerups;
-    public Cosmetic[] cosmetics;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    public Account (String username, char[] pass) {
+public class Account {
+    @JsonProperty("username")
+    private String username;
+    @JsonProperty("password")
+    private String pass;
+    @JsonProperty("stats")
+    private LevelStatistic levelStats;
+    @JsonProperty("progress")
+    public PlayerProgress playerProgress;
+    @JsonProperty("coins")
+    public int coins;
+    //public Sprite playerSprite;
+    //public Powerup[] powerups;
+    //public Cosmetic[] cosmetics;
+
+    /*@JsonCreator
+    public Account(@JsonProperty("username") String username,
+        @JsonProperty("password") String password,
+        @JsonProperty("coins") int coins) {
+        this.username = username;
+        this.pass = password;
+        this.coins = coins;
+    }*/
+
+    @JsonCreator
+    public Account(@JsonProperty("username") String username,
+        @JsonProperty("password") String password,
+        @JsonProperty("coins") int coins,
+        @JsonProperty("stats") LevelStatistic levelStats,
+        @JsonProperty("progress") PlayerProgress playerProgress) {
+        this.username = username;
+        this.pass = password;
+        this.coins = coins;
+        this.levelStats = levelStats;
+        this.playerProgress = playerProgress;
+    }
+
+    public Account (String username, String pass) {
         this.username = username;
         this.pass = pass;
+        this.levelStats = new LevelStatistic();
+        this.playerProgress = new PlayerProgress();
+        coins = 0;
     }
 
     public String getUsername() {
         return username;
     }
 
-    protected char[] getPassword() {
+    protected String getPassword() {
         return pass;
     }
 
-    protected void setPassword(char[] newPass) {
+    protected void setPassword(String newPass) {
         this.pass = newPass;
+        Parental.saveAccountData();
     }
 
     public LevelStatistic getStats() {
         return levelStats;
     }
 
+    public void setStats(LevelStatistic levelStats) {
+        this.levelStats = levelStats;
+    }
+
+    public void setStats(int wordsPerMinute, int mistakes, int highscore, int attempts, Level_status status) {
+        this.levelStats.updateStats(wordsPerMinute, mistakes, highscore, attempts, status);
+    }
+
     public PlayerProgress getProgress() {
         return playerProgress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if ( (o != null) && (o instanceof Account) ) {
+            Account otherAccount = (Account) o;
+            if (this.username.equals(otherAccount.getUsername())) {
+                if (this.pass.equals(otherAccount.getPassword())) {return true;}
+            }
+        }
+        return false;
     }
 }
