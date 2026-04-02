b553f904241ec445a3892147951220c7cb2fdd52
Osman Mohammed Idris <oidris@uwo.ca>
2026-04-02 15:31:31 -0400

    leveldata class for testing

---
+++ src/main/java/ca/uwo/cs2212/group54/stayingalive/accounts/LevelData.java
@@ -0,0 +1,21 @@
+package ca.uwo.cs2212.group54.stayingalive.accounts;
+
+public class LevelData {
+    private int section;
+    private int levelNum;
+    
+    public LevelData() {
+
+    }
+
+    public int getSection() {
+        return section;
+    }
+
+    public int getLevelNum() {
+        return levelNum;
+    }
+
+    
+
+}
